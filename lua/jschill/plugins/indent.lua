9bdbc202e2c456a5205c8c543ee80ec4cd75ff7c
Justin Schilleman <justin@schilleman.com>
2023-05-04 18:59:24 -0400

    Adjusted indent to exclude dashboard, and add to init (forgot previously)

--- lua/jschill/plugins/indent.lua
+++ lua/jschill/plugins/indent.lua
@@ -3,4 +3,14 @@ if not status then
 	return
 end
 
+vim.g.indent_blankline_use_treesitter = true
+vim.g.indent_blankline_filetype_exclude = {
+	"lspinfo",
+	"packer",
+	"checkhealth",
+	"help",
+	"man",
+	"dashboard",
+}
+
 indent.setup({})
