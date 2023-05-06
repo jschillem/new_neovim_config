8c5e47b68899043c719cc3dcf254d9fdf99cdc30
Justin Schilleman <justin@schilleman.com>
2023-05-05 23:39:18 -0400

    Made mason update on run.

--- lua/jschill/plugins-setup.lua
+++ lua/jschill/plugins-setup.lua
@@ -67,7 +67,7 @@ return packer.startup(function(use)
 	use("rafamadriz/friendly-snippets")
 
 	-- managing and installing lsp servers
-	use("williamboman/mason.nvim")
+	use({ "williamboman/mason.nvim", run = ":MasonUpdate" })
 	use("williamboman/mason-lspconfig.nvim")
 
 	-- configuring lsp servers
