1018619db2d8d7fe86ecb357d209c85c66b03ed9
Justin Schilleman <justin@schilleman.com>
2023-05-06 00:25:27 -0400

    Add hop keybinds for find.

--- lua/jschill/plugins/hop.lua
+++ lua/jschill/plugins/hop.lua
@@ -3,6 +3,11 @@ if not status then
 	return
 end
 
+local dir_status, directions = pcall(require, "hop.hint")
+if not dir_status then
+	return
+end
+
 local keymap = vim.keymap
 
 hop.setup({
@@ -11,3 +16,16 @@ hop.setup({
 
 keymap.set("n", "<leader>h", ":HopWord<CR>")
 keymap.set("n", "<leader>H", ":HopLineStart<CR>")
+
+keymap.set("", "f", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, { remap = true })
+keymap.set("", "F", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, { remap = true })
+keymap.set("", "t", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, { remap = true })
+keymap.set("", "T", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, { remap = true })
