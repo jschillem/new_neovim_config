989220868968e2a5c812e97387445d22ab2303bc
Justin Schilleman <justin@schilleman.com>
2024-04-08 10:58:35 -0400

    change copilot binds + start copilot disabled

--- lua/jschillem/plugins/copilot.lua
+++ lua/jschillem/plugins/copilot.lua
@@ -11,6 +11,10 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<leader>ct", ":Copilot toggle<CR>", { desc = "Toggle Copilot" })
+		-- Start disabled
+		vim.cmd("Copilot disable")
+
+		vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { desc = "Enable Copilot" })
+		vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = "Disable Copilot" })
 	end,
 }
